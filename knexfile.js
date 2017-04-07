3c40e3cfef6276aac19ae07bda94f92b38e730b2
nicowave <geoidlife@gmail.com>
2017-04-06 22:22:00 -0600

    fixed knexfile.js and package.json for dependecies and production environment syntax

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
   test: {
     client: 'pg',
     connection: 'postgres://localhost/bookshelf_test'
-  }
+  },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
